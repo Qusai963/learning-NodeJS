defdd4181b3bd500709a4b965e8db6848292a46a
Qusai963 <qusaiarmoush610@gmail.com>
2023-05-07 01:23:57 +0300

    Middleware

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('inside first middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('inside second middleware');
+    res.send('<h1>Hello world!</h1>');
+});
 const server = http.createServer(app);
 
 server.listen(3000);
