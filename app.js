ea8586901c38004c83b5cf232947d1f6a9adfc14
Qusai963 <qusaiarmoush610@gmail.com>
2023-05-08 18:03:35 +0300

    styles

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views/page-not-found.html'));;
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));;
 });
 
 app.listen(3000);
