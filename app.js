f4d077875fd19c9815ab97bdc0f885acaab157d5
Qusai963 <qusaiarmoush610@gmail.com>
2023-05-08 13:29:30 +0300

    Error handling

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Not Found</h1>');
+});
 
 app.listen(3000);
