54b1601b4a994103fbeba4a320243c228cefe284
Qusai963 <qusaiarmoush610@gmail.com>
2023-05-11 15:29:13 +0300

    add ejs files

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,22 @@
 const path = require('path');
 
-const rootDir = require('../util/path');
-
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const adminData = require('./admin')
-
 router.get('/', (req, res, next) => {
-   console.log(adminData.products);
-    res.render('shop', {prods: adminData.products, pageTitle: 'shop', path: '/'});
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
+});
 
-  module.exports = router;
+module.exports = router;
